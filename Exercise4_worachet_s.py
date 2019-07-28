1f27dbbd54c961424d92d3455b078f6f17f23eb4
worachet027 <52350420+worachet027@users.noreply.github.com>
2019-07-28 22:19:12 +0700

    Add files via upload

---
+++ Exercise4_worachet_s.py
@@ -0,0 +1,9 @@
+d=79
+c=98
+b=88
+a=74
+print("------Your Score------")
+print("Foundation English",":",d)
+print("General Business",":",c)
+print("Introduction to Computer",":",b)
+print("Computer Programming",":",a)
